fb3fee24682cdef0d0f89114a1a42c827a3d8603
RISHABH MISHRA <143255867+0Rishabh@users.noreply.github.com>
2026-02-19 10:23:08 +0530

    Added Pandas groupby to calculate total sales by department
    This script calculates the total sales for each department using a DataFrame.

---
+++ total_sales_by_department.py
@@ -0,0 +1,33 @@
+
+
+
+# =========================================
+# Problem: Calculate total sales
+# for each department
+# =========================================
+
+# Import libraries
+import pandas as pd
+import numpy as np
+
+# Create DataFrame
+data = {
+    "emp_id": [101,102,103,104,105,106,107,108],
+    "emp_name": ["Amit","Riya","Rahul","Neha","Arjun","Priya","Karan","Meera"],
+    "department": ["IT","HR","IT","Sales","Sales","HR","IT","Sales"],
+    "city": ["Delhi","Mumbai","Bangalore","Delhi","Mumbai","Delhi","Mumbai","Bangalore"],
+    "salary": [50000,45000,60000,40000,38000,47000,52000,41000],
+    "sales": [200,150,300,250,180,np.nan,220,270],
+    "month": ["Jan","Jan","Feb","Feb","Jan","Feb","Jan","Feb"],
+    "experience_years": [2,3,4,1,2,3,2,2]
+}
+
+df = pd.DataFrame(data)
+
+
+# Solution: Total sales by department
+result = df.groupby(
+    "department", as_index=False
+)["sales"].agg(Total_Sales="sum")
+
+print(result)
